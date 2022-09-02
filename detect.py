9883b17c9e3b5f5807c8e04fe75dbca7d3422e79
Nguyễn Văn Phát <nguyenvanphat@MacBook-Pro-cua-Nguyen.local>
2022-09-02 15:43:02 +0700

    hehe

--- detect.py
+++ detect.py
@@ -260,7 +260,7 @@ def detect(save_img=False):
                             pprint("xywh_current", xywh_current)
                             # xywh_previous = det_previous[i][:4]
                             # pprint("xywh_previous", xywh_previous)
-                            pprint("det_previous", det_previous)
+                            pprint("det_previous", det_previous[i])
                             # plot_one_box_center_point(xywh, im0, color=colors[int(cls)], line_thickness=1, det_previous,
                             #                           cls, conf)
                             det_previous = xyxy2xywh_for_det_previous(det)
